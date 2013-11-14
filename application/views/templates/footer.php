676050c001a44cdb6554edf689a3d49e3ba11537
l2ollingneko <l2ollingneko@gmail.com>
2013-11-14 21:58:40 +0700

    Initial view (header, footer)

---
+++ application/views/templates/footer.php
@@ -0,0 +1,11 @@
+	<!-- footer :3 -->
+	<footer style="height: 50px; background-color: #282828;">
+		<div style="padding-top: 15px; text-align: center;">
+			<p style="color: #ffffff;">©&nbsp;&nbsp;&nbsp;2013&nbsp;&nbsp;QTDBLabs</p>
+			<p style="font-family: 'montserrat-regular'; font-size: 16px;">
+			</p>
+		</div>
+	</footer>
+
+	</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<title>Guitar Shop DB</title>
+
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/guitarshop.css">
+
+		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-2.0.3.min.js"></script> 
+		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.js"></script> 
+
+	</head>
+
+	<body>
+
+		<div id="custom-navbar" class="navbar navbar-default navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+					<button id="menu-button" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a id="meow-kling-brand" class="navbar-brand" href="#" style="color: white">Guitar Shop</a>
+				</div>
+				<div class="navbar-collapse collapse">
+					<ul id="ul-custom" class="nav navbar-nav navbar-right">
+						<li><a href="<?php echo base_url(); ?>">Home</a></li>
+						<!-- <li><a href="<?php echo base_url(); ?>cables">cables</a></li> -->
+						<!-- <li><a href="<?php echo base_url(); ?>faq">faq</a></li> -->
+						<!-- <li><a href="<?php echo base_url(); ?>about">about</a></li> -->
+						<!-- <li><a href="<?php echo base_url(); ?>contact">contact</a></li> -->
+					</ul>
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+		
